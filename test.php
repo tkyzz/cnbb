4c26481f08d4b52652f26132895a6f5f566f4d75
tkyzz <zhuyicui@163.com>
2016-12-24 00:08:50 +0800

    add

---
+++ test.php
@@ -0,0 +1,61 @@
+<?php 
+//echo urldecode('http%3A%2F%2Fb.cainiaobangbang.com');
+
+//echo md5("Sm14ZVZLR1lQN3hZRnBZTW1ndE9IQT09_A_19ad48c6d7");
+//
+//
+//
+//
+//
+//
+//echo '密文: MTQ3MzE1MTAy<br>';
+//
+ echo base64_encode('111111790');
+// echo '距离明文更近的密文: '.base64_decode('MTQ3NjA2OTg3');
+// echo '距离明文更近的密文: '.base64_decode('MTQ3NjA2Nzcy');
+// echo '距离明文更近的密文: '.base64_decode('MTQ3NjA2OTU=');
+// echo '距离明文更近的密文: '.base64_decode('MTQ3NjA2OTY=');
+// echo '距离明文更近的密文: '.base64_decode('MTQ3Njc3OTg5');
+//phpinfo();
+// echo '<pre>';
+// print_r($_SERVER);
+// require './ThinkPHP/Common/functions.php';
+//     /**
+//      * @desc 随机手机
+//      */
+//     function randMobile()
+//     {
+//         $arr = array(139,138,137,136,135,134,159,158,150,151,152,157,188,187,130,131,132,155,156,133,153,189,180);
+//         $head =  $arr[array_rand($arr)];
+//         $chars_array = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+//         $charsLen = count($chars_array) - 1;
+
+//         $outputstr = "";
+//         for ($i=0; $i<8; $i++)
+//         {
+//             $outputstr .= $chars_array[mt_rand(0, $charsLen)];
+//         }
+//         return $head.$outputstr;
+//     }
+// // $url = 'http://www.hishixi.com/Search/lenovoKeyword';
+// // $data = ['t' => '实习'];
+// // //for($i=0;$i<1000;$i++){
+// // 	$res = request_api($url,$data,'post'); //岗位
+// // 	//echo $i;
+// // //}
+// // echo '<pre>';
+// // print_r($res);
+// 				$key_arr = [
+// 					'access_token' => 'Sm14ZVZLR1lQN3hZRnBZTW1ndE9IQT09_A_a8e312c102',
+// 					'src' => 1 ,
+// 					'keyword' => 'test'
+// 					];
+// 				//$data['username'] = randMobile();
+// 				$url = 'http://www.shanxinhui.net/Comp/Company/sendVerifyCode';
+// 					//echo get_api_info('post',"search_keyword_count");
+// 			for($i=0;$i<50;$i++){
+// 				$data['username'] = randMobile();
+// 				echo $data['username'].'<br>';
+// 				//echo request_api($url,$data,'post'); 
+// 			}
+ ?>
